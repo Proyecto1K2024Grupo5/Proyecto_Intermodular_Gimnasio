77193b10ed89e30be9d28401a0bb17346fbd9a10
Fernando <Miemail>
2025-01-16 17:49:21 +0100

    methods done

--- src/Gimnasio/Acceso.java
+++ src/Gimnasio/Acceso.java
@@ -8,4 +8,52 @@ public class Acceso {
     private String fechaSalida;
 
 
+
+    public Acceso(int codigo, String nif, String feEn, String fecSa) {
+
+        this.codigoGimnasio = codigo;
+        this.nifCliente = nif;
+        this.fechaEntrada = feEn;
+        this.fechaSalida = fecSa;
+    }
+
+
+
+
+        public String toJson() {
+        StringBuilder jsonBuilder = new StringBuilder();
+
+        jsonBuilder.append("{\n")
+            .append("\t \"CodigoGimnasio\": ").append(codigoGimnasio).append(",\n")
+            .append("\t \"nifCliente\": ").append(nifCliente).append(",\n")
+            .append("\t \"fechaEntrada\": ").append(fechaEntrada).append(",\n")
+            .append("\t \"fechaSalida\": ").append(fechaSalida).append(",\n")
+                .append("}");
+
+        return jsonBuilder.toString();
+
+        }
+
+    public String toXml() {
+        StringBuilder jsonBuilder = new StringBuilder();
+
+        jsonBuilder.append("<Acceso>\n")
+                .append("\t <CodigoGimnasio>: ").append(codigoGimnasio).append("</CodigoGimnasio>\n")
+                .append("\t <nifCliente>: ").append(nifCliente).append("</nifCliente>\n")
+                .append("\t <fechaEntrada>: ").append(fechaEntrada).append("</fechaEntrada>\n")
+                .append("\t <fechaSalida>: ").append(fechaSalida).append("</fechaSalida>\n")
+                .append("</Acceso>");
+
+        return jsonBuilder.toString();
+
+
+
+
+    }
+
+
+
+
+
+
 }
--- src/Gimnasio/Clases.java
+++ src/Gimnasio/Clases.java
@@ -4,4 +4,26 @@ public class Clases {
 
     private int codigoClases;
 
+
+    public String toJson() {
+        StringBuilder jsonBuilder = new StringBuilder();
+
+        jsonBuilder.append("{\n")
+                .append("\t \"codigoClases\": ").append(codigoClases).append(",\n")
+                .append("}");
+
+        return jsonBuilder.toString();
+
+    }
+
+    public String toXml() {
+        StringBuilder jsonBuilder = new StringBuilder();
+
+        jsonBuilder.append("<Acceso>\n")
+                .append("\t <codigoClases>: ").append(codigoClases).append("</codigoClases>\n")
+                .append("</Acceso>");
+
+        return jsonBuilder.toString();
+
+    }
 }
